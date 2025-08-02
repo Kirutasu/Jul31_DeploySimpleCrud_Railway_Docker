265227907068313b8b42876547823e85f7cd21aa
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-08-02 19:53:20 +0200

    fix: Usa RedirectView para la ruta raiz en urls.py para evitar ImproperlyConfigured

--- sistema_libros/urls.py
+++ sistema_libros/urls.py
@@ -1,7 +1,8 @@
+# En tu urls.py principal del proyecto
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect # <--- ¡Cambiado!
-
+# from django.shortcuts import redirect # <-- Ya no necesitas esta si usas RedirectView
+from django.views.generic.base import RedirectView # <--- ¡Nueva importación!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +10,6 @@ urlpatterns = [
             path('libros/', include('libros.urls')),
             path('categorias/', include('categorias.urls')),
     ])),
-    # --- Añade esta línea para redirigir la raíz ---
-    path('', redirect('/v1/libros/', permanent=True)), # Redirige la raíz a /v1/libros/
+    # --- Añade esta línea para la redirección de la raíz usando RedirectView ---
+    path('', RedirectView.as_view(url='/v1/libros/', permanent=True)),
 ]
